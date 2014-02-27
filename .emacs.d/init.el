ce69d1048a9c7ef1bdba7654787df77cd9bbd7a1
Tom Hancock <thomas.hancock@swiftkey.net>
2014-02-27 16:33:11 +0000

    Update emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,8 @@
-(require 'color-theme)
-    (color-theme-initialize)
-    (color-theme-dark-laptop)
+(load-theme 'manoj-dark t)
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
 
 (require 'font-lock)
 
