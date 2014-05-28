7115215c1e692975665423ef5774c49df046211a
Tom Hancock <thomas.hancock@swiftkey.net>
2014-05-28 15:50:36 +0100

    New emacs fun

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,15 @@
 (load-theme 'dark-laptop t)
 
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(dolist (p '(multiple-cursors
+             paredit))
+  (when (not (package-installed-p p))
+    (package-install p)))
+
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
@@ -17,17 +27,14 @@
 (global-set-key [(control right)] 'end-of-line)
 (global-set-key [(control up)] 'beginning-of-buffer)
 (global-set-key [(control down)] 'end-of-buffer)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/unmark-next-like-this)
 
 (setq c-basic-offset 4)
 
 (add-to-list 'load-path "~/.emacs.d/window-number")
 (require 'window-number)
 (window-number-meta-mode)
-
-(require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
-(package-initialize)
 
 (setq auto-mode-alist
       (cons '("SConstruct" . python-mode) auto-mode-alist))
@@ -37,6 +44,8 @@
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq indent-line-function 'insert-tab)
+
+(add-hook 'clojure-mode-hook 'paredit-mode)
 
 (setq locale-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
